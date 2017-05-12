e2f6088806618756a7faac04dc302d73192bbf6b
Valentin Wannesian <valentin.wanne@gmail.com>
2017-05-12 11:11:42 +0200

    Test case for user story "Frame";

---
+++ src/main.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class main {
+	
+	public void frame(){
+		System.out.println("Frame Function");
+		
+		
+	}
+}
---
+++ src/mainTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class mainTest {
+
+	@Test
+	public void testFrame() {
+		System.out.println("-- Testing Frames --");
+		main instance = new main();
+		int[] frameResult = new int[2];
+		frameResult = instance.frame();
+		
+		int totalValue = frameResult[0] + frameResult[1];
+		
+		assertTrue(totalValue <= 10);
+		
+		for(int p = 0; p < 2; p++){
+			System.out.println(frameResult[p]);
+		}
+	}
+
+}
