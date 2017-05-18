4f601f9a931b177ca0e843da984aa8a07aaf3080
Frank <frankyboy440@gmail.com>
2017-05-18 22:43:36 +0200

    Implementation of user story "Multiple Frames"

--- src/main.java
+++ src/main.java
@@ -39,15 +39,29 @@ public class main {
 	public int gameScore(int[][] game) {
 		int score = 0;
 		for(int i = 0; i < 10; i++) {
+			// If i is a strike
 			if(isStrike(game[i][0], game[i][1])) {
 				score += 10;
-				if(i+1 < 10)
-					score += game[i+1][0] + game[i+1][1];
-			} else if(isSpare(game[i][0], game[i][1])) {
+				// If there's a frame after i, check for another strike and increase score according to user stories
+				if(i+1 < 10) {
+					// Check for secondary strike
+					if(isStrike(game[i+1][0], game[i+1][1])) {
+						score += 10;
+						// check if there's a frame after this strike
+						if(i+2 < 10) {
+							// and add the first throw in that frame
+							score += game[i+2][0];
+						}
+					} else {
+						score += game[i+1][0] + game[i+1][1];
+					}
+				}
+			} else if(isSpare(game[i][0], game[i][1])) { // If i is a spare
+				// Add 10 and if there's a frame after i, add the first throw from that frame
 				score += 10;
 				if(i+1 < 10)
 					score += game[i+1][0];
-			} else {
+			} else { // if i is anything else
 				score += game[i][0] + game[i][1];
 			}
 		}
